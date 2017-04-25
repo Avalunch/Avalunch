fe9ce1fa7a9f14ea799397e23fa181064d0cf344
SoraZodia <sorazodia@users.noreply.github.com>
2017-04-25 10:52:34 -1000

    Database setup

--- app/client/main.js
+++ app/client/main.js
@@ -1,6 +1,6 @@
 import '/imports/startup/client';
 import '/imports/startup/both';
-import '/imports/api/contacts';
+import '/imports/api/list';
 import '/imports/ui/layouts';
 import '/imports/ui/pages';
 import '/imports/ui/templates/';
