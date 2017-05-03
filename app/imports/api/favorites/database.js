531a253f33f055ac542cdc0f18cb54e68bec4243
SoraZodia <sorazodia@users.noreply.github.com>
2017-05-02 18:41:40 -1000

    proof of concept

--- app/imports/api/favorites/database.js
+++ app/imports/api/favorites/database.js
@@ -1,29 +1,5 @@
 import { Mongo } from 'meteor/mongo';
-import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 /* eslint-disable object-shorthand */
 
 export const Favorites = new Mongo.Collection('Favorites');
-
-export const Schema = new SimpleSchema({
-  name: {
-    label: 'name',
-    type: String,
-    optional: false,
-    max: 20,
-  },
-  info: {
-    label: 'info',
-    type: String,
-    optional: false,
-    max: 100,
-  },
-  price: {
-    label: 'price',
-    type: String,
-    optional: false,
-    max: 40,
-  },
-});
-
-Favorites.attachSchema(Schema);
--- app/imports/ui/pages/favorites.js
+++ app/imports/ui/pages/favorites.js
@@ -1,5 +1,33 @@
 import { Template } from 'meteor/templating';
-import { Favorites, Schema } from '/imports/api/favorites/database.js';
+import { Favorites } from '/imports/api/favorites/database.js';
+
+const sample = [
+  {
+    name: 'Eggplant Parmesan',
+    icon: 'images/meteor-logo.png',
+    price: '$4.00',
+  },
+  {
+    name: 'Thai Red Vegetable Curry',
+    icon: 'images/meteor-logo.png',
+    price: '$4.00',
+  },
+  {
+    name: 'Vege Parmesan',
+    icon: 'images/meteor-logo.png',
+    price: '$4.00',
+  },
+];
 
 Template.Favorites_Page.onCreated(function onCreated() {
+  this.subscribe('Favorites');
+  _.each(sample, function insert(index) {
+    Favorites.insert(index);
+  });
+});
+
+Template.Favorites_Page.helpers({
+  list() {
+    return Favorites.find();
+  },
 });
